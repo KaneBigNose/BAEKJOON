4eda59610e2855a52d9732d66ce2339168d89a3e
KaneBigNose <imddongs@naver.com>
2024-08-10 20:54:48 +0900

    실버5-단어정렬

--- BaekJoon/main.cpp
+++ BaekJoon/main.cpp
@@ -5,35 +5,27 @@ using namespace std;
 
 int main()
 {
-	int t;
-	cin >> t;
-	for (int i = 0; i < t; i++)
+	int n;
+	cin >> n;
+	vector<string> arr[51];
+	for (int i = 0; i < n; i++)
 	{
-		vector<int> num1, num2;
-		int n, m;
-		cin >> n >> m;
-		unsigned long long result1 = 1, result2 = 1;
-		for (int j = 0; j < n; j++)
+		string temp;
+		cin >> temp;
+		if (find(arr[temp.size()].begin(), arr[temp.size()].end(), temp) == arr[temp.size()].end())
 		{
-			num1.push_back(m - j);
+			arr[temp.size()].push_back(temp);
 		}
-		for (int j = 0; j < n; j++)
+	}
+	for (int i = 0; i < 51; i++)
+	{
+		sort(arr[i].begin(), arr[i].end());
+	}
+	for (int i = 0; i < 51; i++)
+	{
+		for (int j = 0; j < arr[i].size(); j++)
 		{
-			num2.push_back(n - j);
+			cout << arr[i][j] << "\n";
 		}
-		for (int j = 0; j < n; j++)
-		{
-			if (find(num1.begin(), num1.end(), n - j) != num1.end())
-			{
-				*find(num1.begin(), num1.end(), n - j) = 1;
-				*find(num2.begin(), num2.end(), n - j) = 1;
-			}
-		}
-		for (int j = 0; j < n; j++)
-		{
-			result1 *= num1[j];
-			result2 *= num2[j];
-		}
-		cout << result1 / result2 << "\n";
 	}
 }
