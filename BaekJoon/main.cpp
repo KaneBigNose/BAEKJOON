989c0445411724e178451e9378a474bd0759ad1d
KaneBigNose <imddongs@naver.com>
2024-08-12 20:31:12 +0900

    실버5-메세지

--- BaekJoon/main.cpp
+++ BaekJoon/main.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 #include <vector>
-#include <string>
 using namespace std;
 
 int main()
 {
-	int senario = 1;
+	int GroupNum = 1;
 	while (true)
 	{
+		bool everybody = true;
 		int n;
 		cin >> n;
 		if (n == 0)
@@ -15,28 +15,44 @@ int main()
 			break;
 		}
 		vector<string> name;
-		cin.ignore();
+		vector<char>* PN = new vector<char>[n];
 		for (int i = 0; i < n; i++)
 		{
 			string temp;
-			getline(cin, temp);
+			cin >> temp;
 			name.push_back(temp);
-		}
-		vector<char>* AB = new vector<char>[n];
-		for (int i = 0; i < 2 * n - 1; i++)
-		{
-			int num;
-			char temp;
-			cin >> num >> temp;
-			AB[num - 1].push_back(temp);
-		}
-		for (int i = 0; i < n; i++)
-		{
-			if (AB[i].size() == 1)
+			for (int j = 0; j < n - 1; j++)
 			{
-				cout << senario << " " << name[i] << "\n";
+				char message;
+				cin >> message;
+				PN[i].push_back(message);
 			}
 		}
-		senario++;
+		cout << "Group " << GroupNum << "\n";
+		for (int i = 0; i < n; i++)
+		{
+			for (int j = 0; j < n - 1; j++)
+			{
+				if (PN[i][j] == 'N')
+				{
+					if (i - (j + 1) < 0)
+					{
+						cout << name[i - (j + 1) + n] << " was nasty about ";
+					}
+					else
+					{
+						cout << name[i - (j + 1)] << " was nasty about ";
+					}
+					cout << name[i] << "\n";
+					everybody = false;
+				}
+			}
+		}
+		if (everybody == true)
+		{
+			cout << "Nobody was nasty\n";
+		}
+		cout << "\n";
+		GroupNum++;
 	}
 }
