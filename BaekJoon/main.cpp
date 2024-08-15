4744ddd4d6fff7401a2b560a568336fa8e2a3119
KaneBigNose <imddongs@naver.com>
2024-08-15 20:09:46 +0900

    실버4 - 좋은구간

--- BaekJoon/main.cpp
+++ BaekJoon/main.cpp
@@ -1,55 +1,57 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
 int main()
 {
-	int n, kim, lim;
-	cin >> n >> kim >> lim;
-	int round = 1;
-	while (true)
+	int l, n;
+	cin >> l;
+	vector<int> s;
+	for (int i = 0; i < l; i++)
 	{
-		if (kim == n && n % 2 == 1)
-		{
-			goto A;
-		}
-		else if (lim == n && n % 2 == 1)
-		{
-			goto A;
-		}
-		if (kim % 2 == 1 && kim + 1 == lim)
-		{
-			break;
-		}
-		else if (kim % 2 == 0 && kim - 1 == lim)
-		{
-			break;
-		}
-	A:
-		if (kim % 2 == 1)
-		{
-			kim = kim / 2 + 1;
-		}
-		else
-		{
-			kim = kim / 2;
-		}
-		if (lim % 2 == 1)
-		{
-			lim = lim / 2 + 1;
-		}
-		else
-		{
-			lim = lim / 2;
-		}
-		if (n % 2 == 1)
-		{
-			n = n / 2 + 1;
-		}
-		else
-		{
-			n = n / 2;
-		}
-		round++;
+		int temp;
+		cin >> temp;
+		s.push_back(temp);
 	}
-	cout << round;
+	cin >> n;
+	sort(s.begin(), s.end());
+	int first, last, count = 0;
+	for (int i = 0; i < l; i++)
+	{
+		if (n == s[i])
+		{
+			cout << "0";
+			return 0;
+		}
+		else if (n < s[i])
+		{
+			if (n < s[0]) // n이 집합의 원소들보다 작을 경우
+			{
+				first = 1;
+			}
+			else if (i - 1 <= 0)
+			{
+				first = s[0] + 1;
+			}
+			else
+			{
+				first = s[i - 1] + 1;
+			}
+			last = s[i] - 1;
+			break;
+		}
+	}
+	for (int i = first; i <= n; i++)
+	{
+		for (int j = n; j <= last; j++)
+		{
+			count++;
+		}
+	}
+	if (l != 1) // [n, n] 구간이 발생하여 이를 제외
+	{
+		count--;
+	}
+	cout << count;
 }
