4d7cdf7a35fc5290a2bc54e7ed5ef4f228fdbc11
KaneBigNose <imddongs@naver.com>
2024-08-15 17:27:04 +0900

    실버4 - 기타줄

--- BaekJoon/main.cpp
+++ BaekJoon/main.cpp
@@ -5,27 +5,51 @@ using namespace std;
 
 int main()
 {
-	int n;
-	cin >> n;
-	vector<int> A, B;
+	int n, m;
+	cin >> n >> m;
+	vector<int> package, single;
+	vector<int> price;
+	for (int i = 0; i < m; i++)
+	{
+		int temp1, temp2;
+		cin >> temp1 >> temp2;
+		package.push_back(temp1);
+		single.push_back(temp2);
+	}
+	sort(package.begin(), package.end(), less<>());
+	sort(single.begin(), single.end(), less<>());
+	int num = n;
 	int sum = 0;
-	for (int i = 0; i < n; i++)
+	while (true)
 	{
-		int temp;
-		cin >> temp;
-		A.push_back(temp);
+		if (num <= 0)
+		{
+			price.push_back(sum);
+			break;
+		}
+		sum += package[0];
+		num -= 6;
 	}
-	for (int i = 0; i < n; i++)
+	num = n;
+	sum = 0;
+	while(true)
 	{
-		int temp;
-		cin >> temp;
-		B.push_back(temp);
+		if (num <= 0)
+		{
+			price.push_back(sum);
+			break;
+		}
+		if (num >= 6)
+		{
+			sum += package[0];
+			num -= 6;
+		}
+		else if (num > 0 && num < 6)
+		{
+			sum += single[0];
+			num -= 1;
+		}
 	}
-	sort(A.begin(), A.end(), less<>());
-	sort(B.begin(), B.end(), greater<>());
-	for (int i = 0; i < n; i++)
-	{
-		sum += A[i] * B[i];
-	}
-	cout << sum;
+	price.push_back(single[0] * n);
+	cout << *min_element(price.begin(), price.end());
 }
