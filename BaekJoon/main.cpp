398336e676e2ae5e992664b0c8e5fe33b91a4702
KaneBigNose <imddongs@naver.com>
2024-08-17 20:06:53 +0900

    실버4 - 문자열

--- BaekJoon/main.cpp
+++ BaekJoon/main.cpp
@@ -1,40 +1,70 @@
 #include <iostream>
-#include <string>
 using namespace std;
 
 int main()
 {
-	int n;
-	cin >> n;
-	int count = 0;
-	for (int i = 0; i < n; i++)
+	string x, y;
+	cin >> x >> y;
+	if (x.size() == y.size())
 	{
-		string temp = to_string(i + 1);
-		if (temp.size() == 1 || temp.size() == 2)
+		int count = 0;
+		for (int i = 0; i < x.size(); i++)
 		{
-			count++;
-		}
-		else
-		{
-			string a, b;
-			a = temp[0];
-			b = temp[1];
-			int sub = stoi(a) - stoi(b);
-			bool same = true;
-			for (int j = 1; j < temp.size() - 1; j++)
-			{
-				a = temp[j];
-				b = temp[j + 1];
-				if (sub != stoi(a) - stoi(b))
-				{
-					same = false;
-				}
-				sub = stoi(a) - stoi(b);
-			}
-			if (same == true)
+			if (x[i] != y[i])
 			{
 				count++;
 			}
+		}
+		cout << count;
+		return 0;
+	}
+	string final = x;
+	for (int i = 0; i <= y.size() - x.size(); i++)
+	{
+		final += " ";
+	}
+	for (int i = 0; i <= y.size() - x.size(); i++)
+	{
+		string temp;
+		int countFinal = 0, countTemp = 0;
+		for (int j = 0; j < i; j++)
+		{
+			temp += " ";
+		}
+		temp += x;
+		for (int j = 0; j < y.size() - x.size() - i; j++)
+		{
+			temp += " ";
+		}
+		for (int i = 0; i < y.size(); i++)
+		{
+			if (temp[i] == y[i])
+			{
+				countTemp++;
+			}
+		}
+		for (int i = 0; i < y.size(); i++)
+		{
+			if (final[i] == y[i])
+			{
+				countFinal++;
+			}
+		}
+		if (countFinal < countTemp)
+		{
+			final = temp;
+		}
+	}
+	int count = 0;
+	for (int i = 0; i < final.size(); i++)
+	{
+		if (final[i] == ' ')
+		{
+			final[i] = y[i];
+		}
+		if (final[i] != y[i])
+		{
+			count++;
 		}
 	}
 	cout << count;
