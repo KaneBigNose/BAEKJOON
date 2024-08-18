65f0e5045af2c7818b90dcd91098e2a9a9c0d326
KaneBigNose <imddongs@naver.com>
2024-08-18 17:58:56 +0900

    실버4 - 요세푸스 문제(실패)
    알고리즘은 맞으나 시간 초과 문제 발생 시간복잡도를 공부하고 나서 다시 도전

--- BaekJoon/main.cpp
+++ BaekJoon/main.cpp
@@ -1,71 +1,47 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
 int main()
 {
-	string x, y;
-	cin >> x >> y;
-	if (x.size() == y.size())
-	{
-		int count = 0;
-		for (int i = 0; i < x.size(); i++)
-		{
-			if (x[i] != y[i])
-			{
-				count++;
-			}
-		}
-		cout << count;
-		return 0;
-	}
-	string final = x;
-	for (int i = 0; i <= y.size() - x.size(); i++)
-	{
-		final += " ";
-	}
-	for (int i = 0; i <= y.size() - x.size(); i++)
-	{
-		string temp;
-		int countFinal = 0, countTemp = 0;
-		for (int j = 0; j < i; j++)
-		{
-			temp += " ";
-		}
-		temp += x;
-		for (int j = 0; j < y.size() - x.size() - i; j++)
-		{
-			temp += " ";
-		}
-		for (int i = 0; i < y.size(); i++)
-		{
-			if (temp[i] == y[i])
-			{
-				countTemp++;
-			}
-		}
-		for (int i = 0; i < y.size(); i++)
-		{
-			if (final[i] == y[i])
-			{
-				countFinal++;
-			}
-		}
-		if (countFinal < countTemp)
-		{
-			final = temp;
-		}
-	}
+	int n, k;
+	cin >> n >> k;
+	vector<int> arr;
+	int current = k;
 	int count = 0;
-	for (int i = 0; i < final.size(); i++)
+	arr.push_back(k);
+	while (true)
 	{
-		if (final[i] == ' ')
+		if (arr.size() == n)
 		{
-			final[i] = y[i];
+			break;
 		}
-		if (final[i] != y[i])
+		if (current + 1 > n)
+		{
+			current = 1;
+		}
+		else
+		{
+			current++;
+		}
+		if (find(arr.begin(), arr.end(), current) == arr.end())
 		{
 			count++;
 		}
+		if (count >= k)
+		{
+			if (find(arr.begin(), arr.end(), current) == arr.end())
+			{
+				arr.push_back(current);
+				count = 0;
+			}
+		}
 	}
-	cout << count;
+	cout << "<";
+	for (int i = 0; i < n - 1; i++)
+	{
+		cout << arr[i] << ", ";
+	}
+	cout << *(arr.end() - 1) << ">";
 }
