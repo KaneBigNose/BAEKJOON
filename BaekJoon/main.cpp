636aee74aa9f9e875bdd3602d0ec6e9578c464a8
KaneBigNose <imddongs@naver.com>
2024-08-18 20:55:30 +0900

    실버4 - 스위치 켜고 끄기(실패)
    내일 재도전

--- BaekJoon/main.cpp
+++ BaekJoon/main.cpp
@@ -1,41 +1,101 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 using namespace std;
+
+void ChangeSwitch(vector<int>& switchNum, int num)
+{
+	if (switchNum[num] == 0)
+	{
+		switchNum[num] = 1;
+	}
+	else
+	{
+		switchNum[num] = 0;
+	}
+}
 
 int main()
 {
 	int n;
 	cin >> n;
-	vector<string> num;
+	vector<int> switchNum;
 	for (int i = 0; i < n; i++)
 	{
-		string temp;
+		int temp;
 		cin >> temp;
-		num.push_back(temp);
-		reverse(num[i].begin(), num[i].end());
+		switchNum.push_back(temp);
 	}
-	int current = 0;
-	int size = num[0].size();
-	while (true)
+	cin >> n;
+	for (int i = 0; i < n; i++)
 	{
-		if (num[0].size() == 0)
+		int gender, num;
+		cin >> gender >> num;
+		if (gender == 1) // 남성
 		{
-			cout << "1";
-			break;
-		}
-		for (int i = 0; i < n; i++)
-		{
-			if (count(num.begin(), num.end(), num[i]) > 1)
+			int j = 1;
+			while (true)
 			{
-				cout << size - current + 1;
-				return 0;
+				if (num * j <= switchNum.size())
+				{
+					ChangeSwitch(switchNum, num * j - 1);
+					j++;
+				}
+				else
+				{
+					break;
+				}
 			}
 		}
-		for (int i = 0; i < n; i++)
+		else // 여성
 		{
-			num[i].pop_back();
+			ChangeSwitch(switchNum, num - 1);
+			int j = 1;
+			while (true)
+			{
+				if (num - j - 1 >= 0 && num + j - 1 < switchNum.size())
+				{
+					if (switchNum[num - j - 1] == switchNum[num + j - 1])
+					{
+						ChangeSwitch(switchNum, num - j - 1);
+						ChangeSwitch(switchNum, num + j - 1);
+					}
+				}
+				else
+				{
+					break;
+				}
+				j++;
+			}
 		}
-		current++;
+	}
+	if (switchNum.size() <= 20)
+	{
+		for (int i = 0; i < switchNum.size(); i++)
+		{
+			cout << switchNum[i] << " ";
+		}
+	}
+	else
+	{
+		int j = 0;
+		int k = 1;
+		while (true)
+		{
+			if (k * 20 > switchNum.size())
+			{
+				for (int i = j; i < switchNum.size(); i++)
+				{
+					cout << switchNum[i] << " ";
+				}
+				break;
+			}
+			for (int i = j; i < k * 20; i++)
+			{
+				cout << switchNum[i] << " ";
+			}
+			j = k * 20;
+			k++;
+			cout << "\n";
+		}
 	}
 }
