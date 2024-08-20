f158c732c1baafa9afb5726f7e9b71ade8f0f931
KaneBigNose <imddongs@naver.com>
2024-08-20 17:50:43 +0900

    실버4 - 나이트 투어

--- BaekJoon/main.cpp
+++ BaekJoon/main.cpp
@@ -1,31 +1,61 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <string>
+#include <cmath>
 using namespace std;
 
 int main()
 {
-	int n;
-	cin >> n;
-	vector<string> bestseller;
-	for (int i = 0; i < n; i++)
+	vector<string> visit;
+	string beforeNum;
+	string currentNum;
+	for (int i = 0; i < 36; i++)
 	{
 		string temp;
 		cin >> temp;
-		bestseller.push_back(temp);
+		visit.push_back(temp);
 	}
-	sort(bestseller.begin(), bestseller.end(), less<>());
-	vector<int> selling;
-	for (int i = 0; i < n; i++)
+	string first, last;
+	first = visit[0][1];
+	last = visit[35][1];
+	if (abs((int)visit[0][0] - (int)visit[35][0]) == 2 && abs(stoi(first) - stoi(last)) == 1) {} // 마지막으로 방문한 지점에서 처음 지점으로 갈 수 있는지 체크
+	else if(abs((int)visit[0][0] - (int)visit[35][0]) == 1 && abs(stoi(first) - stoi(last)) == 2) {}
+	else // 갈 수 없는 경우
 	{
-		selling.push_back(count(bestseller.begin(), bestseller.end(), bestseller[i]));
+		cout << "Invalid";
+		return 0;
 	}
-	for (int i = 0; i < n; i++)
+	for (int i = 0; i < visit.size() - 1; i++)
 	{
-		if (selling[i] == *max_element(selling.begin(), selling.end()))
+		beforeNum = visit[i][1];
+		currentNum = visit[i + 1][1];
+		if (count(visit.begin(), visit.end(), visit[i]) != 1) // 이미 갔던 지점을 재방문한 경우
 		{
-			cout << bestseller[i];
-			break;
+			cout << "Invalid";
+			return 0;
+		}
+		if (abs(stoi(beforeNum) - stoi(currentNum)) == 2) // 위 아래 방향으로 2칸 이동 후 90도 이동
+		{
+			if (abs((int)visit[i][0] - (int)visit[i + 1][0]) != 1)
+			{
+				cout << "Invalid";
+				return 0;
+			}
+		}
+		else if (abs((int)visit[i][0] - (int)visit[i + 1][0]) == 2) // 좌 우 방향으로 2칸 이동후 90도 이동
+		{
+			if (abs(stoi(beforeNum) - stoi(currentNum)) != 1)
+			{
+				cout << "Invalid";
+				return 0;
+			}
+		}
+		else // 나이트의 이동 방식이 아닌 경우
+		{
+			cout << "Invalid";
+			return 0;
 		}
 	}
+	cout << "Valid";
 }
