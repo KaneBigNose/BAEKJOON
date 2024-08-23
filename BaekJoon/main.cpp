9c90bce38828b5b2df00b1e656dcf913932bce34
KaneBigNose <imddongs@naver.com>
2024-08-23 20:25:23 +0900

    실버3 - 터렛
    재도전해서 성공

--- BaekJoon/main.cpp
+++ BaekJoon/main.cpp
@@ -1,38 +1,59 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 int main()
 {
-	int n, m; // 세로 가로
-	cin >> n >> m;
-	int north = -1, south = n, west = -1, east = m; // 각 방향의 끝
-	int nowX = 0, nowY = 0; // 현 위치
-	while (true)
+	int t;
+	cin >> t;
+	for (int i = 0; i < t; i++)
 	{
-		nowY = south - 1;
-		west++;
-		if (nowX + 1 == east)
+		int x1, y1, r1, x2, y2, r2;
+		cin >> x1 >> y1 >> r1 >> x2 >> y2 >> r2;
+		int bigR = r1 > r2 ? r1 : r2;
+		int smallR = r1 <= r2 ? r1 : r2;
+		int distance2 = pow(x1 - x2, 2) + pow(y1 - y2, 2); // 중심 사이 거리의 제곱
+		if (x1 == x2 && y1 == y2 && r1 == r2) // 원의 중심이 동일하고 반지름도 동일한 경우
 		{
-			break;
+			cout << "-1\n";
 		}
-		nowX = east - 1;
-		south--;
-		if (nowY - 1 == north)
+		else if (x1 == x2 && y1 == y2 && r1 != r2) // 원의 중심이 동일하고 반지름이 다른 경우
 		{
-			break;
+			cout << "0\n";
 		}
-		nowY = north + 1;
-		east--;
-		if (nowX - 1 == west)
+		else if (distance2 > pow(bigR, 2)) // 외부인 경우
 		{
-			break;
+			if (distance2 > pow(r1 + r2, 2)) // 접점이 0개인 경우
+			{
+				cout << "0\n";
+			}
+			else if (distance2 == pow(r1 + r2, 2)) // 접점이 1개인 경우
+			{
+				cout << "1\n";
+			}
+			else if (distance2 < pow(r1 + r2, 2)) // 접점이 2개인 경우
+			{
+				cout << "2\n";
+			}
 		}
-		nowX = west + 1;
-		north++;
-		if (nowY + 1 == south)
+		else if (distance2 == pow(bigR, 2)) // 경계선인 경우
 		{
-			break;
+			cout << "2\n";
+		}
+		else if (distance2 < pow(bigR, 2)) // 내부인 경우
+		{
+			if (sqrt(distance2) + smallR > bigR)
+			{
+				cout << "2\n";
+			}
+			if (sqrt(distance2) + smallR == bigR)
+			{
+				cout << "1\n";
+			}
+			if (sqrt(distance2) + smallR < bigR)
+			{
+				cout << "0\n";
+			}
 		}
 	}
-	cout << nowY << " " << nowX;
 }
