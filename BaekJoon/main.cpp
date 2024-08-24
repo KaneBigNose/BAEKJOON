583f53c2f9bf64897ef66113f232ec8421640295
KaneBigNose <imddongs@naver.com>
2024-08-24 21:03:08 +0900

    실버3 - 피보나치 함수(실패)
    실패사유 : 시간초과 메모이제이션을 활용하였으나 실패

--- BaekJoon/main.cpp
+++ BaekJoon/main.cpp
@@ -1,6 +1,67 @@
 #include <iostream>
-#include <cmath>
 using namespace std;
+
+int callzero[41] = { 0, };
+int callone[41] = { 0, };
+
+void fibonacci(int now, int original)
+{
+	if (callzero[now] != 0 || callone[now] != 0)
+	{
+		return;
+	}
+	if (now == 0)
+	{
+		callzero[original]++;
+	}
+	else if (now == 1)
+	{
+		callone[original]++;
+	}
+	else
+	{
+		if (now - 2 == 0)
+		{
+			callzero[original]++;
+		}
+		else if (now - 2 == 1)
+		{
+			callone[original]++;
+		}
+		else
+		{
+			if (callzero[now - 2] == 0 && callone[now - 2] == 0)
+			{
+				fibonacci(now - 2, original);
+			}
+			else
+			{
+				callzero[original] += callzero[now - 2];
+				callone[original] += callone[now - 2];
+			}
+		}
+		if (now - 1 == 0)
+		{
+			callzero[original]++;
+		}
+		else if (now - 1 == 1)
+		{
+			callone[original]++;
+		}
+		else
+		{
+			if (callzero[now - 1] == 0 && callone[now - 1] == 0)
+			{
+				fibonacci(now - 1, original);
+			}
+			else
+			{
+				callzero[original] += callzero[now - 1];
+				callone[original] += callone[now - 1];
+			}
+		}
+	}
+}
 
 int main()
 {
@@ -8,52 +69,9 @@ int main()
 	cin >> t;
 	for (int i = 0; i < t; i++)
 	{
-		int x1, y1, r1, x2, y2, r2;
-		cin >> x1 >> y1 >> r1 >> x2 >> y2 >> r2;
-		int bigR = r1 > r2 ? r1 : r2;
-		int smallR = r1 <= r2 ? r1 : r2;
-		int distance2 = pow(x1 - x2, 2) + pow(y1 - y2, 2); // 중심 사이 거리의 제곱
-		if (x1 == x2 && y1 == y2 && r1 == r2) // 원의 중심이 동일하고 반지름도 동일한 경우
-		{
-			cout << "-1\n";
-		}
-		else if (x1 == x2 && y1 == y2 && r1 != r2) // 원의 중심이 동일하고 반지름이 다른 경우
-		{
-			cout << "0\n";
-		}
-		else if (distance2 > pow(bigR, 2)) // 외부인 경우
-		{
-			if (distance2 > pow(r1 + r2, 2)) // 접점이 0개인 경우
-			{
-				cout << "0\n";
-			}
-			else if (distance2 == pow(r1 + r2, 2)) // 접점이 1개인 경우
-			{
-				cout << "1\n";
-			}
-			else if (distance2 < pow(r1 + r2, 2)) // 접점이 2개인 경우
-			{
-				cout << "2\n";
-			}
-		}
-		else if (distance2 == pow(bigR, 2)) // 경계선인 경우
-		{
-			cout << "2\n";
-		}
-		else if (distance2 < pow(bigR, 2)) // 내부인 경우
-		{
-			if (sqrt(distance2) + smallR > bigR)
-			{
-				cout << "2\n";
-			}
-			if (sqrt(distance2) + smallR == bigR)
-			{
-				cout << "1\n";
-			}
-			if (sqrt(distance2) + smallR < bigR)
-			{
-				cout << "0\n";
-			}
-		}
+		int temp;
+		cin >> temp;
+		fibonacci(temp, temp);
+		cout << callzero[temp] << " " << callone[temp] << "\n";
 	}
 }
