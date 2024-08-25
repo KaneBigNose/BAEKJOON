b22739c7227b0d06050fe15fc03016613278cbb9
KaneBigNose <imddongs@naver.com>
2024-08-25 18:45:56 +0900

    실버3 - 어린 왕자

--- BaekJoon/main.cpp
+++ BaekJoon/main.cpp
@@ -1,77 +1,42 @@
 #include <iostream>
+#include <vector>
+#include <cmath>
 using namespace std;
-
-int callzero[41] = { 0, };
-int callone[41] = { 0, };
-
-void fibonacci(int now, int original)
-{
-	if (callzero[now] != 0 || callone[now] != 0)
-	{
-		return;
-	}
-	if (now == 0)
-	{
-		callzero[original]++;
-	}
-	else if (now == 1)
-	{
-		callone[original]++;
-	}
-	else
-	{
-		if (now - 2 == 0)
-		{
-			callzero[original]++;
-		}
-		else if (now - 2 == 1)
-		{
-			callone[original]++;
-		}
-		else
-		{
-			if (callzero[now - 2] == 0 && callone[now - 2] == 0)
-			{
-				fibonacci(now - 2, original);
-			}
-			else
-			{
-				callzero[original] += callzero[now - 2];
-				callone[original] += callone[now - 2];
-			}
-		}
-		if (now - 1 == 0)
-		{
-			callzero[original]++;
-		}
-		else if (now - 1 == 1)
-		{
-			callone[original]++;
-		}
-		else
-		{
-			if (callzero[now - 1] == 0 && callone[now - 1] == 0)
-			{
-				fibonacci(now - 1, original);
-			}
-			else
-			{
-				callzero[original] += callzero[now - 1];
-				callone[original] += callone[now - 1];
-			}
-		}
-	}
-}
 
 int main()
 {
-	int t;
+	int t; // 테스트 케이스
 	cin >> t;
 	for (int i = 0; i < t; i++)
 	{
-		int temp;
-		cin >> temp;
-		fibonacci(temp, temp);
-		cout << callzero[temp] << " " << callone[temp] << "\n";
+		int x1, y1, x2, y2; // 시작 좌표, 도착 좌표
+		cin >> x1 >> y1 >> x2 >> y2;
+		int n; // 행성의 개수
+		cin >> n;
+		vector<int> x, y, r; // 행성의 x, y 좌표 및 반지름
+		for (int j = 0; j < n; j++)
+		{
+			int temp1, temp2, temp3;
+			cin >> temp1 >> temp2 >> temp3;
+			x.push_back(temp1);
+			y.push_back(temp2);
+			r.push_back(temp3);
+		}
+		// 시작 좌표나 도착 좌표가 어떤 행성 내에 있으면 반드시 통과해야함
+		int count = 0;
+		for (int j = 0; j < n; j++)
+		{
+			int distance1 = pow(x1 - x[j], 2) + pow(y1 - y[j], 2); // 거리의 제곱
+			int distance2 = pow(x2 - x[j], 2) + pow(y2 - y[j], 2); // 거리의 제곱
+			if (distance1 < pow(r[j], 2) && distance2 > pow(r[j], 2))
+			{
+				count++;
+			}
+			else if (distance1 > pow(r[j], 2) && distance2 < pow(r[j], 2))
+			{
+				count++;
+			}
+		}
+		cout << count << "\n";
 	}
 }
