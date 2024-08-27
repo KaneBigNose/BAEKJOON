b4d5882a9a10423448e6bed6ab2d48cceee208e5
KaneBigNose <imddongs@naver.com>
2024-08-27 19:40:42 +0900

    실버3 - 킹
    코드가 상당히 김

--- BaekJoon/main.cpp
+++ BaekJoon/main.cpp
@@ -1,39 +1,119 @@
 #include <iostream>
-#include <vector>
-#include <cmath>
+#include <string>
 using namespace std;
+
+int changetoint(char word)
+{
+	switch (word)
+	{
+	case 'A':
+		return 1;
+	case 'B':
+		return 2;
+	case 'C':
+		return 3;
+	case 'D':
+		return 4;
+	case 'E':
+		return 5;
+	case 'F':
+		return 6;
+	case 'G':
+		return 7;
+	case 'H':
+		return 8;
+	}
+}
+
+char changetochar(int num)
+{
+	switch (num)
+	{
+	case 1:
+		return 'A';
+	case 2:
+		return 'B';
+	case 3:
+		return 'C';
+	case 4:
+		return 'D';
+	case 5:
+		return 'E';
+	case 6:
+		return 'F';
+	case 7:
+		return 'G';
+	case 8:
+		return 'H';
+	}
+}
+
+void move(string& king, string& stone, int up, int right)
+{
+	string temp1, temp2;
+	int king1, stone1;
+	temp1 = king[1];
+	temp2 = stone[1];
+	king1 = stoi(temp1);
+	stone1 = stoi(temp2);
+	if (king1 + up > 8 || king1 + up < 1 || changetoint(king[0]) + right > 8 || changetoint(king[0]) + right < 1)
+	{
+		return;
+	}
+	if (changetoint(king[0]) + right == changetoint(stone[0]) && king1 + up == stone1)
+	{
+		if (stone1 + up > 8 || stone1 + up < 1 || changetoint(stone[0]) + right > 8 || changetoint(stone[0]) + right < 1)
+		{
+			return;
+		}
+		stone[0] = changetochar(changetoint(stone[0]) + right);
+		stone[1] = stone[1] + up;
+	}
+	king[0] = changetochar(changetoint(king[0]) + right);
+	king[1] = king[1] + up;
+}
 
 int main()
 {
-	int n, m;
-	cin >> n >> m;
-	vector<string> arr;
+	string king, stone;
+	int n;
+	cin >> king >> stone >> n;
 	for (int i = 0; i < n; i++)
 	{
-		string temp;
-		cin >> temp;
-		arr.push_back(temp);
-	}
-	int maxsize = n > m ? m : n;
-	maxsize--;
-	while (true)
-	{
-		if (maxsize <= 0)
+		string act;
+		cin >> act;
+		if (act == "R")
 		{
-			cout << "1";
-			break;
+			move(king, stone, 0, 1);
 		}
-		for (int i = 0; i < n - maxsize; i++)
+		else if (act == "L")
 		{
-			for (int j = 0; j < m - maxsize; j++)
-			{
-				if (arr[i][j] == arr[i][j + maxsize] && arr[i][j + maxsize] == arr[i + maxsize][j] && arr[i + maxsize][j] == arr[i + maxsize][j + maxsize])
-				{
-					cout << pow(maxsize + 1, 2);
-					return 0;
-				}
-			}
+			move(king, stone, 0, -1);
 		}
-		maxsize--;
+		else if (act == "T")
+		{
+			move(king, stone, 1, 0);
+		}
+		else if (act == "B")
+		{
+			move(king, stone, -1, 0);
+		}
+		else if (act == "RT")
+		{
+			move(king, stone, 1, 1);
+		}
+		else if (act == "LT")
+		{
+			move(king, stone, 1, -1);
+		}
+		else if (act == "RB")
+		{
+			move(king, stone, -1, 1);
+		}
+		else if (act == "LB")
+		{
+			move(king, stone, -1, -1);
+		}
 	}
+	cout << king << "\n" << stone;
 }
