7b66132747ddbfa52b128ecefdce1f433783d130
KaneBigNose <imddongs@naver.com>
2024-08-30 21:11:19 +0900

    실버3 - 수리공 항승(실패)

--- BaekJoon/main.cpp
+++ BaekJoon/main.cpp
@@ -5,8 +5,8 @@ using namespace std;
 
 int main()
 {
-	int n;
-	cin >> n;
+	int n, l;
+	cin >> n >> l;
 	vector<int> arr;
 	for (int i = 0; i < n; i++)
 	{
@@ -14,23 +14,29 @@ int main()
 		cin >> temp;
 		arr.push_back(temp);
 	}
-	sort(arr.begin(), arr.end(), greater<>());
-	int big = 0;
-	while (true)
+	sort(arr.begin(), arr.end(), less<>());
+	int countT = 0;
+	int now = 0;
+	while(true)
 	{
-		for (int i = big + 1; i < n - 1; i++)
+		for (int i = now + 1; i < n; i++)
 		{
-			if (arr[big] < arr[i] + arr[i + 1])
+			if (arr[i] - arr[now] + 1 > l)
 			{
-				cout << arr[big] + arr[i] + arr[i + 1];
+				countT++;
+				now = i;
+				break;
+			}
+			if (arr[now + 1] - arr[now] + 1 <= l && i == n - 1)
+			{
+				cout << ++countT;
 				return 0;
 			}
 		}
-		if (big == n - 3)
+		if (now >= n - 1)
 		{
-			cout << "-1";
 			break;
 		}
-		big++;
 	}
+	cout << countT;
 }
