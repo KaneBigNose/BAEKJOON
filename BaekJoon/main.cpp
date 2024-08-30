d84364f4cfb105124e474ed968e1f3a1532334d2
KaneBigNose <imddongs@naver.com>
2024-08-30 20:08:08 +0900

    실버3 - 삼각형 만들기

--- BaekJoon/main.cpp
+++ BaekJoon/main.cpp
@@ -3,69 +3,34 @@
 #include <algorithm>
 using namespace std;
 
-void stringFomula(string word, int& sum)
-{
-	for (int i = 0; i < word.size(); i++)
-	{
-		if ((int)word[i] >= 48 && (int)word[i] <= 57)
-		{
-			sum += (int)word[i] - 48;
-		}
-	}
-}
-
-void comparator(vector<string>& arr, int i, int j)
-{
-	int sum1 = 0, sum2 = 0;
-	stringFomula(arr[i], sum1);
-	stringFomula(arr[j], sum2);
-	if (sum1 > sum2)
-	{
-		swap(arr[i], arr[j]);
-	}
-	else if (sum1 == sum2)
-	{
-		if (arr[i] > arr[j])
-		{
-			swap(arr[i], arr[j]);
-		}
-	}
-}
-
 int main()
 {
 	int n;
 	cin >> n;
-	vector<string> arr;
+	vector<int> arr;
 	for (int i = 0; i < n; i++)
 	{
-		string temp;
+		int temp;
 		cin >> temp;
 		arr.push_back(temp);
 	}
-	for (int i = 0; i < arr.size() - 1; i++)
+	sort(arr.begin(), arr.end(), greater<>());
+	int big = 0;
+	while (true)
 	{
-		for (int j = i; j < arr.size(); j++)
+		for (int i = big + 1; i < n - 1; i++)
 		{
-			if (arr[i].size() > arr[j].size())
+			if (arr[big] < arr[i] + arr[i + 1])
 			{
-				swap(arr[i], arr[j]);
+				cout << arr[big] + arr[i] + arr[i + 1];
+				return 0;
 			}
 		}
-	}
-	for (int i = 0; i < arr.size() - 1; i++)
-	{
-		for (int j = i + 1; j < arr.size(); j++)
+		if (big == n - 3)
 		{
-			if (arr[i].size() == arr[j].size())
-			{
-				comparator(arr, i, j);
-			}
+			cout << "-1";
+			break;
 		}
+		big++;
 	}
-	for (int i = 0; i < arr.size() - 1; i++)
-	{
-		cout << arr[i] << "\n";
-	}
-	cout << *(arr.end() - 1);
 }
