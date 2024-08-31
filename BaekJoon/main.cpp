8b61d5dbb855c4365848dabeb2e34332ec425cfe
KaneBigNose <imddongs@naver.com>
2024-08-31 20:16:34 +0900

    실버3 - 1로 만들기(실패)

--- BaekJoon/main.cpp
+++ BaekJoon/main.cpp
@@ -3,84 +3,28 @@ using namespace std;
 
 int main()
 {
-	unsigned long long x, y;
-	int w, s;
-	cin >> x >> y >> w >> s;
-	bool cross;
-	unsigned long long time = 0;
-	if (w * 2 >= s)
+	int x;
+	cin >> x;
+	int count = 0;
+	while (true)
 	{
-		cross = true;
-	}
-	else
-	{
-		cross = false;
-	}
-	if (x > y)
-	{
-		if (cross == true)
+		if (x == 1)
 		{
-			time += s * y;
+			break;
+		}
+		if (x % 3 == 0)
+		{
+			x = x / 3;
+		}
+		else if (x % 2 == 0)
+		{
+			x = x / 2;
 		}
 		else
 		{
-			time += w * 2 * y;
+			x--;
 		}
-		if ((x - y) % 2 == 0)
-		{
-			if ((x - y) * s < (x - y) * w)
-			{
-				time += (x - y) * s;
-			}
-			else
-			{
-				time += (x - y) * w;
-			}
-		}
-		else
-		{
-			if ((x - y - 1) * s + w < (x - y) * w)
-			{
-				time += (x - y - 1) * s + w;
-			}
-			else
-			{
-				time += (x - y) * w;
-			}
-		}
+		count++;
 	}
-	else
-	{
-		if (cross == true)
-		{
-			time += s * x;
-		}
-		else
-		{
-			time += w * 2 * x;
-		}
-		if ((y - x) % 2 == 0)
-		{
-			if ((y - x) * s < (y - x) * w)
-			{
-				time += (y - x) * s;
-			}
-			else
-			{
-				time += (y - x) * w;
-			}
-		}
-		else
-		{
-			if ((y - x - 1) * s + w < (y - x) * w)
-			{
-				time += (y - x - 1) * s + w;
-			}
-			else
-			{
-				time += (y - x) * w;
-			}
-		}
-	}
-	cout << time;
+	cout<<count;
 }
