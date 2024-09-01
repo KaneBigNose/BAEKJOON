265eac4bc1dba64383ea3c8a74f77aab35025a08
KaneBigNose <imddongs@naver.com>
2024-09-01 21:13:32 +0900

    실버2 - 유기농 배추

--- BaekJoon/main.cpp
+++ BaekJoon/main.cpp
@@ -1,30 +1,71 @@
 #include <iostream>
+#include <vector>
 using namespace std;
+
+void searchBC(vector<bool>* check, vector<int>* arr, int a, int b, int m, int n)
+{
+	arr[a][b] = 0;
+	check[a][b] = true;
+	if (a - 1 >= 0 && arr[a - 1][b] == 1 && check[a - 1][b] == false) // 북
+	{
+		searchBC(check, arr, a - 1, b, m, n);
+	}
+	if (b + 1 < m && arr[a][b + 1] == 1 && check[a][b + 1] == false) // 동
+	{
+		searchBC(check, arr, a, b + 1, m, n);
+	}
+	if (a + 1 < n && arr[a + 1][b] == 1 && check[a + 1][b] == false) // 남
+	{
+		searchBC(check, arr, a + 1, b, m, n);
+	}
+	if (b - 1 >= 0 && arr[a][b - 1] == 1 && check[a][b - 1] == false) // 서
+	{
+		searchBC(check, arr, a, b - 1, m, n);
+	}
+}
 
 int main()
 {
-	int x;
-	cin >> x;
-	int count = 0;
-	while (true)
+	int t;
+	cin >> t;
+	for (int i = 0; i < t; i++)
 	{
-		if (x == 1)
+		int m, n, k; // 가로, 세로, 개수
+		cin >> m >> n >> k;
+		vector<bool>* check = new vector<bool>[n];
+		vector<int>* arr = new vector<int>[n];
+		int bugcount = 0;
+		for (int a = 0; a < n; a++)
 		{
-			break;
+			for (int b = 0; b < m; b++)
+			{
+				check[a].push_back(false);
+			}
 		}
-		if (x % 3 == 0)
+		for (int a = 0; a < n; a++)
 		{
-			x = x / 3;
+			for (int b = 0; b < m; b++)
+			{
+				arr[a].push_back(0);
+			}
 		}
-		else if (x % 2 == 0)
+		for (int j = 0; j < k; j++)
 		{
-			x = x / 2;
+			int x, y;
+			cin >> x >> y;
+			arr[y][x] = 1;
 		}
-		else
+		for (int a = 0; a < n; a++)
 		{
-			x--;
+			for (int b = 0; b < m; b++)
+			{
+				if (arr[a][b] == 1 && check[a][b] == false)
+				{
+					searchBC(check, arr, a, b, m, n);
+					bugcount++;
+				}
+			}
 		}
-		count++;
+		cout << bugcount << "\n";
 	}
-	cout<<count;
 }
